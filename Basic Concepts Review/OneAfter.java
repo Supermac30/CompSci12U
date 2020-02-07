e0acfb6abe3124162a5a4fb5ecfcc980a8eb404b
Mark Bedaywi <50279151+Supermac30@users.noreply.github.com>
2020-02-07 13:00:06 -0500

    Fixed error after misreading question

--- Basic Concepts Review/OneAfter.java
+++ Basic Concepts Review/OneAfter.java
@@ -5,8 +5,9 @@ public class OneAfter {
         System.out.println(oneAfter("abc", "abXabc"));
     }
     public static boolean oneAfter(String word0, String word1) {
-        boolean cond0 = word0.substring(word0.length()-3).toLowerCase().equals(word1.toLowerCase());
-        boolean cond1 = word1.substring(word1.length()-3).toLowerCase().equals(word0.toLowerCase());
-        return cond0 || cond1;
+        if (word0.length() > word1.length()) {
+            return word0.substring(word0.length()-word1.length()).toLowerCase().equals(word1.toLowerCase());
+        }
+        return word1.substring(word1.length()-word0.length()).toLowerCase().equals(word0.toLowerCase());
     }
 }
